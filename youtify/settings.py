e5e940f87861ffa9d8f236a37bd3f8db8b2310c2
GowthamanLL <140971610+GowthamanLL@users.noreply.github.com>
2023-10-26 10:41:01 +0530

    Update settings.py

--- youtify/settings.py
+++ youtify/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
